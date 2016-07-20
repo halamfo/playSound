be3444f829eda74a161a6cb064168684831d2d6e
halamfo <tungnt991@gmail.com>
2016-07-20 16:30:03 +0700

    first committtt

--- Play Sound/ViewController.swift
+++ Play Sound/ViewController.swift
@@ -9,9 +9,13 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
     var audio =  AVAudioPlayer()
     var status = ""
+    
+    @IBOutlet weak var sld_duration: UISlider!
+    @IBOutlet weak var lbl_timeremain: UILabel!
+    @IBOutlet weak var lbl_currenttime: UILabel!
     @IBOutlet weak var btn_play: UIButton!
     @IBAction func actionplay(sender: AnyObject) {
         if status == "play"{
@@ -32,10 +36,45 @@ class ViewController: UIViewController {
         sldvolume.setThumbImage(UIImage(named: "thumbHightLight.png"), forState: .Highlighted)
     }
     
+    @IBAction func actionsld_duration(sender: UISlider) {
+        audio.stop()
+        audio.currentTime = NSTimeInterval(sld_duration.value)
+        audio.prepareToPlay()
+        btn_play.setImage(UIImage(named: "pause.png"), forState: .Normal)
+        audio.play()
+        status = "play"
+    }
+    func updateTimeleft(){
+        lbl_currenttime.text = String(format: "%02.0f", (audio.currentTime/60) % 60) + ":" + String(format: "%02.0f", audio.currentTime % 60)
+        lbl_timeremain.text = String(format: "%02.0f",((audio.duration - audio.currentTime)/60)%60) + ":" + String(format: "%02.0f",((audio.duration - audio.currentTime)%60))
+        sld_duration.value  = Float(audio.currentTime)
+    }
+    @IBAction func `switch`(sender: UISwitch) {
+        if sender.on {
+            audio.numberOfLoops = 1
+        }
+        else{
+            audio.numberOfLoops = 0
+        }
+    }
+    func audioPlayerDidFinishPlaying(player: AVAudioPlayer, successfully flag: Bool) {
+        if audio.numberOfLoops == 0{
+            audio.stop()
+            btn_play.setImage(UIImage(named: "play.png"), forState: .Normal)
+            status = "pause"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         audio = try! AVAudioPlayer(contentsOfURL: (NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("music", ofType: ".mp3")!)))
         audio.prepareToPlay()
+        _ = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: #selector(updateTimeleft), userInfo: nil, repeats: true)
+        audio.delegate = self
+        audio.numberOfLoops = 1
+        sld_duration.maximumValue = Float(audio.duration)
+        sld_duration.setThumbImage(UIImage(named: "duration.png"), forState: .Normal)
+
     }
 }
 
